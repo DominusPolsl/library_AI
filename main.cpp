21596160c14e1b9157bc4c5365695e6eecbcb23b
Bohdan Kostyk <bk318913@student.polsl.pl>
2025-05-26 20:32:38 +0200

    function func in main.cpp was added

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <QApplication>
 #include "mainwindow.h"
 using namespace std;
+double func(double, double);
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
+    func(3.14, 5.43);
     MainWindow window;
     window.setWindowTitle("Multimedia Library Menu");
     window.resize(600, 400);
@@ -13,3 +15,11 @@ int main(int argc, char *argv[]) {
 
     return app.exec();
 }
+double func(double a, double b){
+    double res = 1;
+    for (int i = 0; i < b; i++){
+        res = res * a;
+    }
+    return res;
+}
+
